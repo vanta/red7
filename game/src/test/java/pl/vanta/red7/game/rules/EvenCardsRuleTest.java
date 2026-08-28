d2a96a5d6d15d48d09b93aae97ff69909b27e14e
Krzysztof Wolny <k.wolny@smartrecruiters.com>
2026-08-28 23:14:51 +0200

    Rule tests

---
+++ game/src/test/java/pl/vanta/red7/game/rules/EvenCardsRuleTest.java
@@ -0,0 +1,35 @@
+package pl.vanta.red7.game.rules;
+
+import java.util.Set;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import pl.vanta.red7.game.Card;
+import pl.vanta.red7.game.Rule;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.of;
+
+class EvenCardsRuleTest extends RuleBaseTest {
+
+    private final Rule underTest = new EvenCardsRule();
+
+    static Stream<Arguments> testCases() {
+        return Stream.of(
+                of("all red cards", Set.of(R7, R6, R5, R4, R3, R2, R1), Set.of(R6, R4, R2)),
+                of("all 7 cards", Set.of(R7, O7, Y7, G7, B7, I7, V7), Set.of()),
+                of("different colors and numbers", Set.of(V7, O7, R7, R6), Set.of(R6)),
+                of("single card", Set.of(R4), Set.of(R4)),
+                of("low cards only", Set.of(R1, R2, R3), Set.of(R2))
+        );
+    }
+
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("testCases")
+    void shouldReturnEvenCards(String description, Set<Card> input, Set<Card> expected) {
+        assertEquals(expected, underTest.getCardsForRule(input));
+    }
+
+}
