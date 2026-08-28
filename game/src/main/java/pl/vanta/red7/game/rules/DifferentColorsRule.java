7e6bf825d3acb112816d3ec6edb43b5c99f9ae61
Krzysztof Wolny <k.wolny@smartrecruiters.com>
2026-08-28 23:37:50 +0200

    Rule tests

--- game/src/main/java/pl/vanta/red7/game/rules/DifferentColorsRule.java
+++ game/src/main/java/pl/vanta/red7/game/rules/DifferentColorsRule.java
@@ -5,6 +5,9 @@ import java.util.Set;
 import pl.vanta.red7.game.Card;
 import pl.vanta.red7.game.Rule;
 
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toSet;
+
 public class DifferentColorsRule implements Rule {
     @Override
     public String getName() {
@@ -13,6 +16,10 @@ public class DifferentColorsRule implements Rule {
 
     @Override
     public Set<Card> getCardsForRule(Set<Card> cards) {
-        return Set.of();
+        return cards.stream()
+                .collect(groupingBy(Card::color, toSet()))
+                .values().stream()
+                .map(group -> group.stream().max(Card::compareTo).orElseThrow())
+                .collect(toSet());
     }
 }
