05c192d06e81f6f7d0a2cb7ace669a2d2073b4b4
Krzysztof Wolny <k.wolny@smartrecruiters.com>
2026-09-11 19:10:12 +0200

    Added test

--- game/src/test/java/pl/vanta/red7/engine/PlayerTest.java
+++ game/src/test/java/pl/vanta/red7/engine/PlayerTest.java
@@ -3,6 +3,7 @@ package pl.vanta.red7.engine;
 import java.util.Set;
 import java.util.stream.Stream;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -17,6 +18,8 @@ import static org.junit.jupiter.params.provider.Arguments.of;
 
 class PlayerTest extends BaseTest {
 
+    private static final Player PLAYER_ALICE = new Player("Alice");
+
     static Stream<Arguments> handsOfWrongSize() {
         return Stream.of(0, 1, 2, 3, 4, 5, 6, 8, 9, 10, 49)
                 .map(size -> of(size + " cards", Deck.allCards().stream().limit(size).collect(toSet())));
@@ -26,13 +29,25 @@ class PlayerTest extends BaseTest {
     @MethodSource("handsOfWrongSize")
     void shouldNotAllowToInitPlayerWithNot7Cards(String description, Set<Card> hand) {
         //given
-        var player = new Player("Alice");
 
         //when
-        var exception = assertThrows(AssertionError.class, () -> player.init(hand, R7));
+        var exception = assertThrows(AssertionError.class, () -> PLAYER_ALICE.init(hand, R7));
 
         //then
         assertEquals("Player Alice should be dealt 7 cards, but got " + hand.size(), exception.getMessage());
+    }
+
+    @Test
+    void shouldNotAllowToInitPlayerWithTableTheSameAsInHand() {
+        //given
+        var hand = Set.of(R7, R6, R5, R4, R3, R2, R1);
+        var tableCard = R7;
+
+        //when
+        var exception = assertThrows(AssertionError.class, () -> PLAYER_ALICE.init(hand, tableCard));
+
+        //then
+        assertEquals("Player Alice should not have initial card " + tableCard + " in hand", exception.getMessage());
     }
 
 }
